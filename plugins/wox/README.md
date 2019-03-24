28a6ec720bb442549ac437d00b739a06f8c43856
Venyo <venyowang@163.com>
2019-03-24 20:41:54 +0800

    添加 wox 插件

---
+++ plugins/wox/README.md
@@ -0,0 +1,9 @@
+该目录下的项目是 windows 快速启动器 Wox 的一款调用 hotoke 搜索的插件
+
+[Wox 项目地址](https://github.com/qianlifeng/wox)
+
+[插件地址](http://www.wox.one/plugin/268)
+
+使用方法：在 wox 的输入框中输入`ho {keyword}#`如此形式的命令即可自动使用 hotoke 得到搜索结果
+
+`ho`是命令关键词(action keyword)，`{keyword}`为想要搜索的内容，可以包含除了`#`以外的任意字符，因为`#`在此插件中为关键字符，用来标识命令结束，开始进行搜索
---
+++ plugins/wox/src/HotokePlugin4Wox/SearchResultModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotokePlugin4Wox
+{
+    public class SearchResultModel
+    {
+        public string RequestId { get; set; }
+
+        public int Searched { get; set; }
+
+        public bool Finished { get; set; }
+
+        public List<SearchResult> Results { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        private Uri uri;
+        public Uri Uri
+        {
+            get
+            {
+                if (this.uri == null && !string.IsNullOrWhiteSpace(this.Url))
+                {
+                    this.uri = new Uri(this.Url);
+                }
+
+                return this.uri;
+            }
+        }
+
+        public string Desc { get; set; }
+
+        public float Score { get; set; }
+
+        public float Base { get; set; }
+
+        public string Source { get; set; }
+
+        public List<string> Sources { get; set; } = new List<string>();
+    }
+}
