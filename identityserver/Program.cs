0c602c2902c0b53844cf6cdedb11618782b90aa2
Venyo <venyowang@163.com>
2019-03-31 12:43:33 +0800

    添加 jwt 验证

---
+++ identityserver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace JwtIdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls(ConfigurationManager.AppSettings["host"] ?? "http://0.0.0.0:25789")
+                .UseStartup<Startup>();
+    }
+}
