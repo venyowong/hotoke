a64833a433d9aa098b4080678d1e7942f5342709
Venyo <venyowang@163.com>
2019-06-26 19:34:22 +0800

    删除无用代码

--- sql/init.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists hotoke;
-create database hotoke;
-use hotoke;
-create table user(
-    id int auto_increment not null primary key,
-    email varchar(50) not null,
-    password varchar(50) not null,
-    salt varchar(50) not null
-);
