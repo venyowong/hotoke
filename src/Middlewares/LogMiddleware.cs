c481e56c38beeaefef3e02262c709e279fc0be79
venyowong <venyowong@163.com>
2020-07-20 17:21:32 +0800

    日志增加 TraceId

---
+++ src/Middlewares/LogMiddleware.cs
@@ -0,0 +1,30 @@
+namespace Hotoke.Middlewares
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public LogMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            using (LogContext.PushProperty("TraceId", context.TraceIdentifier))
+            {
+                try
+                {
+                    await this.next(context);
+                }
+                catch (Exception e)
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    Log.Error(e, "an error occure");
+                    await context.Response.WriteAsync("an error occure");
+                }
+            }
+        }
+    }
+}
