b1f2a9f1e5fa87eea1a8fe10042b45d8534d1513
venyo wong <venyowong@163.com>
2023-01-04 09:39:48 +0800

    add ToutiaoEngine

---
+++ src/V.Hotoke/Engines/ToutiaoEngine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using V.Talog.Client;
+
+namespace V.Hotoke.Engines
+{
+    public class ToutiaoEngine : GenericEngine
+    {
+        public ToutiaoEngine(IConfiguration config, IHttpClientFactory clientFactory, LogChannel logChannel) 
+            : base("Toutiao", config, clientFactory, logChannel)
+        {
+        }
+
+        public override string GetSearchUrl(string keyword, int pageIndex, bool english)
+        {
+            return this.baseUrl.Replace("{keyword}", System.Web.HttpUtility.UrlEncode(keyword))
+                .Replace("{pageIndex}", pageIndex.ToString());
+        }
+    }
+}
