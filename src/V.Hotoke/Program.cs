99cb3816d2a96c86bc398f803233399c6c7557be
venyo wong <venyowong@163.com>
2023-01-04 09:40:23 +0800

    添加 metrics 上报

--- src/V.Hotoke/Program.cs
+++ src/V.Hotoke/Program.cs
@@ -3,6 +3,7 @@ using Serilog;
 using V.Talog.Client;
 using V.Hotoke.Engines;
 using V.SwitchableCache;
+using V.Hotoke.Services;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
@@ -13,6 +14,11 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Configuration["ReportMetrics"]?.ToLower() == "true")
+{
+    builder.Services.AddHostedService<MetricsHostService>();
+}
 
 // Add services to the container.
 builder.Services.AddControllers()
@@ -37,6 +43,8 @@ builder.Services.AddHttpClient()
     // 既然搜索引擎有反爬机制，代表这个搜索引擎是不希望别人爬取搜索结果的，因此对于有反爬机制的引擎，本项目都不会去使用
     // 而且如果使用各种方式去绕过反爬机制，也都会增加耗时，影响搜索速度
     .AddTransient<ISearchEngine, QuarkSmEngine>()
+    //.AddTransient<ISearchEngine, ToutiaoEngine>()
+    // 头条搜索需要使用 Cookie 才能返回正常结果，但是元搜索引擎有一点就是希望帮用户减少隐私泄露，因此不能启用 Cookie 功能，并且启用之后搜索结果会受到所有使用者的影响
     .AddTransient<MetaSearcher>();
 
 var app = builder.Build();
---
+++ src/V.Hotoke/Services/MetricsHostService.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using V.Talog.Client;
+
+namespace V.Hotoke.Services
+{
+    public class MetricsHostService : IHostedService, IDisposable
+    {
+        private CancellationToken token;
+        private Task task;
+        private volatile bool report = false;
+
+        public MetricsHostService()
+        {
+            this.task = Task.Run(this.ReportMetrics);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.token = cancellationToken;
+            this.report = true;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.report = false;
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            this.task.Dispose();
+        }
+
+        private void ReportMetrics()
+        {
+            while (true)
+            {
+                if (token != default && token.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                if (this.report)
+                {
+                    var beginUsage = Process.GetCurrentProcess().TotalProcessorTime;
+                    var begin = DateTime.Now;
+                    Thread.Sleep(500);
+                    var cpuUsage = (Process.GetCurrentProcess().TotalProcessorTime - beginUsage).TotalMilliseconds;
+                    var passTime = (DateTime.Now - begin).TotalMilliseconds;
+                    var cpuUsagePercent = cpuUsage / passTime / Environment.ProcessorCount * 100;
+                    MetricSender.Enqueue("V.Hotoke", "cpu", (decimal)Math.Round(cpuUsagePercent, 2));
+                    MetricSender.Enqueue("V.Hotoke", "memory", (decimal)Math.Round(Process.GetCurrentProcess().WorkingSet64 / 1024.0 / 1024, 2));
+                }
+
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
